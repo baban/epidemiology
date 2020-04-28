3a1d6c2f7ad72b45272c671776e81826c56dbccf
baban <babanba.n@gmail.com>
2020-04-29 00:47:57 +0900

    コード調整

--- sars-cov2.py
+++ sars-cov2.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
+Nan = np.nan
+
 # 1日をm等分して細かいシミュレーションをする
 m = 7
 # 感染率bと回復率gは以下のurlから取得
@@ -14,9 +16,9 @@ term = 365 # 期間。1年間
 width = term * m # 計算期間
 
 # 日本の人口
-population = 126100000
+population = 126100000.0
 # 感染者の初期値
-inflected_first = 10000.0
+inflected_first = 16.0
 
 suspectiable_delta_list = [0] * width
 infected_delta_list = [0] * width
@@ -27,51 +29,51 @@ infected_list = [0] * width
 recovered_list = [0] * width
 
 # 感染後、発症して感染力を持つまでの期間
-tau = 6
+tau = 6*m
 
 # 無免疫者
-def suspectiable_delta(t):
-  i = t - tau
+def suspectiable_delta(t, diff = 0):
+  i = t - diff
   if i < 0:
     i = 0
-  return b * suspectiable_list[i] * infected_list[i]
+  return b * suspectiable_list[i] * infected_list[i] / population
 
 # 発症中の人
 def infected_delta(t):
-  return suspectiable_delta(t) - recoverd_delta(t)
+  return suspectiable_delta(t, tau) - recoverd_delta(t)
 
 # 回復者
 def recoverd_delta(t):
-  return g * infected_rate
+  return g * infected_list[t]
 
 print("start...")
 
 # 国内に発生したウィルス所持者の初期値
-infected_rate = inflected_first/population
+infected_count = inflected_first
 # 無感染者の割合
-suspectiable_rate = 1.0 - infected_rate
+suspectiable_count = population - infected_count
 # 回復者
-recovered_rate = 0.0
+recovered_count = 0.0
 
 for i in list(range(width)):
+  suspectiable_list[i] = suspectiable_count
+  infected_list[i] = infected_count
+  recovered_list[i] = recovered_count
   suspectiable_delta_list[i] = suspectiable_delta(i)
   recovered_delta_list[i] = recoverd_delta(i)
   infected_delta_list[i] = suspectiable_delta_list[i] - recovered_delta_list[i]
   # print("day:" + str(i+1))
-  # print("新規感染者:" + str(suspectiable_delta_list[i] * population))
-  # print("抗体を得た人:" + str(recovered_delta_list[i] * population))
-  # print("追加の感染者数:" + str(infected_delta_list[i] * population))
-  suspectiable_rate -= suspectiable_delta_list[i]
-  infected_rate += infected_delta_list[i]
-  recovered_rate += recovered_delta_list[i]
-  suspectiable_list[i] = suspectiable_rate
-  infected_list[i] = infected_rate
-  recovered_list[i] = recovered_rate
+  # print("新規感染者:" + str(suspectiable_delta_list[i]))
+  # print("抗体を得た人:" + str(recovered_delta_list[i]))
+  # print("追加の感染者数:" + str(infected_delta_list[i]))
+  suspectiable_count += -1 * suspectiable_delta_list[i]
+  infected_count += infected_delta_list[i]
+  recovered_count += recovered_delta_list[i]
 
 # 日にちをn分割したものを元に戻す
 plot_suspectiable_list = [0] * term
 for i in list(range(width)):
-  plot_suspectiable_list[int(i/m)] += int(suspectiable_delta_list[i]*population)
+  plot_suspectiable_list[int(i/m)] += int(suspectiable_delta_list[i])
 
 for i in list(range(term)):
   print("day:" + str(i+1))
